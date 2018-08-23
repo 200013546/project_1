1394e8a751ccf136d51980c11e28f0be44ecd5b8
Alan McCabe <alan.mccabe@ge.com>
2018-08-23 16:02:49 -0400

    Added lines and state capitals

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,10 +1,6 @@
 var geocoder;
 var map;
 var infowindow = new google.maps.InfoWindow();
-// var formStr = "<input type='text' id='text4mrkr' value='marker text' /><input type='button' value='submit' onclick='addPlace();' />"
-
-// var latitude2 = 33.8725;
-// var longitude2 = -84.3726;
 var latitude2 = 0;
 var longitude2 = 0;
 var oldDistance = 0;
@@ -12,55 +8,9 @@ var num = 1;
 var message = '';
 var val = '';
 var coordinates = [];
-sessionStorage.clear();
+var lineColor = '';
+var lineColors = [];
 
-function chooseTarget() {
-
-  // Get city
-  targetCity = Math.floor(Math.random() * 40) + 1;
-  var address = cities[targetCity].city + "," + cities[targetCity].state;
-  // address = address.replace(" ","+");
-  // address = "Dallas, tx";
-  // console.log(address);
-  // locURL = "https://serpapi.com/search?q=" + address + "&hl=en&gl=us&google_domain=google.com&api_key=AIzaSyBeNH6Wmm34EdS4i5AaMRAhOd2oDqikSW8";
-  // console.log(locURL);
-
-  queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyCwNeT8z4JXX1AvxPPQZVxcBxpAbmqkf0c";
-  // console.log(queryURL);
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-    latitude2 = response.results[0].geometry.location.lat;
-    longitude2 = response.results[0].geometry.location.lng;
-    // console.log(latitude2);
-    // console.log(longitude2);
-    getWeather();
-    // console.log(response.GeocodeResponse.result.geometry.location.lat);
-  });
-}
-
-function getWeather() {
-  // weatherURL = "https://samples.openweathermap.org/data/2.5/weather?lat="+ latitude2 +"&lon="+ longitude2 +"&appid=92ff904996bf0620c63ce50f42eb4c93"
-  weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude2 + "&lon=" + longitude2 + "&appid=92ff904996bf0620c63ce50f42eb4c93"
-  $.ajax({
-    url: weatherURL,
-    method: "GET"
-  }).then(function (response) {
-    temperature = Math.floor((response.main.temp - 273.15) * 1.8) + 32;
-    humidity = response.main.humidity;
-    // console.log(response);
-    // console.log(humidity);
-    // console.log(temperature);
-    // console.log(response);
-    $("#weather-display").text("Temp: " + temperature + " Humidity: " + humidity + "% Weather: " + response.weather[0].description);
-
-  })
-};
-
-
-
-chooseTarget();
 function initialize() {
   map = new google.maps.Map(
     document.getElementById("map_canvas"), {
@@ -79,6 +29,23 @@ function initialize() {
   });
 }
 
+// Initialize the location on the map
+function chooseTarget() {
+  // Get city
+  targetCity = Math.floor(Math.random() * 40) + 1;
+  var address = cities[targetCity].city + "," + cities[targetCity].state;
+  queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyCwNeT8z4JXX1AvxPPQZVxcBxpAbmqkf0c";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    latitude2 = response.results[0].geometry.location.lat;
+    longitude2 = response.results[0].geometry.location.lng;
+    getWeather();
+  });
+}
+
+// Add new place here
 function addPlace() {
   var marker = new google.maps.Marker({
     map: map,
@@ -86,24 +53,27 @@ function addPlace() {
   });
   var newLat = infowindow.getPosition().lat()
   var newLng = infowindow.getPosition().lng()
-  // console.log(newLat);
-  // console.log(newLng);
+  newLat = Math.round(newLat * 1000) / 1000;
+  newLng = Math.round(newLng * 1000) / 1000;
 
-  coordinates = JSON.parse(sessionStorage.getItem("coordinates"));
-  if (coordinates === null) {
-    var coordinates = [];
+  // Put coordinates into array for drawing lines
+  coordinates.push({
+    "lat": newLat,
+    "lng": newLng
+  });
+
+  // Add line colors - loop through coordintes and linecolor array to assign 
+  lineColor = getDistance(newLat, newLng);
+  for (var i = 0; i < coordinates.length - 1; i++) {
+    var flightPath = new google.maps.Polyline({
+      path: [coordinates[i], coordinates[i + 1]],
+      strokeColor: lineColors[i + 1],
+      strokeOpacity: 1.0,
+      strokeWeight: 3
+      //      map: map
+    });
+    flightPath.setMap(map);
   }
-
-  // Put in sessionstorage the guesses with lat and lng to be used for something later
-  var gnum = num - 1;
-  val = "{guess:" + gnum + ",lat:" + newLat + ",lng:" + newLng + "}";
-  coordinates.push(val);
-  sessionStorage.setItem("coordinates", JSON.stringify(coordinates));
-  // Place on page so that we can see them
-  $("#coordinates-display").text(coordinates);
-
-  // Need to find distance for determining Hot/Cold - jump to function
-  getDistance(newLat, newLng);
 
   // Place marker on map
   marker.htmlContent = document.getElementById('text4mrkr').value;
@@ -117,46 +87,61 @@ function addPlace() {
   });
 }
 
-// Not sure if we will use this in future
-function placeLabel(position, map) {
-  var mapLabel = new MapLabel({
-    text: 'Hello!',
-    position: position,
-    map: map,
-    fontSize: 12,
-    align: 'right'
-  });
-}
-
 // Function to find distance and tell hot or cold
 function getDistance(latitude1, longitude1) {
   var distance = google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(latitude1, longitude1), new google.maps.LatLng(latitude2, longitude2));
-  // console.log(distance);
-  // console.log(oldDistance);
   if (distance < 30000) {
     message = "Got it!!";
+    var lineColor = "#00AA00";
     foundIt();
   } else if (oldDistance === 0 && distance > 5000000) {
     message = "Ice Cold!!";
+    var lineColor = "#0000FF";
   } else if (oldDistance === 0) {
     message = "Guess Again!!";
+    var lineColor = "#FFFFFF";
   } else if (oldDistance < distance) {
     message = "Colder";
+    var lineColor = "#00AAFF";
   } else if (oldDistance < distance && distance > 300000) {
     message = "Very Cold!!";
+    var lineColor = "#002AFF";
   } else if (oldDistance > distance && distance < 250000) {
     message = "HOT!!";
+    var lineColor = "#FF0000";
   } else {
     message = "Warmer";
+    var lineColor = "#FF7F00";
   }
   oldDistance = distance;
-  // console.log(message);
   $("#result-display").text(message);
-
+  lineColors.push(lineColor);
+  console.log("LCb: " + lineColor);
+  return lineColor;
 }
 
+// Once location is found, we can let them know
 function foundIt() {
   var gnum = num - 1;
   $("#win-display").text(message + " - " + gnum + " tries!!");
 }
+
+// ACCESS CLUES IN THIS AREA -----------------------------------
+
+// Get weather clue here
+function getWeather() {
+  weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude2 + "&lon=" + longitude2 + "&appid=92ff904996bf0620c63ce50f42eb4c93"
+  $.ajax({
+    url: weatherURL,
+    method: "GET"
+  }).then(function (response) {
+    temperature = Math.floor((response.main.temp - 273.15) * 1.8) + 32;
+    humidity = response.main.humidity;
+    $("#weather-display").text("Temp: " + temperature + " Humidity: " + humidity + "% Weather: " + response.weather[0].description);
+  })
+};
+
+
+// Begin program here to choose target
+chooseTarget();
 google.maps.event.addDomListener(window, "load", initialize);
