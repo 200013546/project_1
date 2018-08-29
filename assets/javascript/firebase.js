97d3d1a0d0e6e831a6441d8a6ed22bb4114cb775
Alan McCabe <alan.mccabe@ge.com>
2018-08-29 18:16:48 -0400

    Various changes

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBz-H7b5a7rmeZ-PmrfPPRegWmXCwchuW8",
+    authDomain: "gegt-alan8-18.firebaseapp.com",
+    databaseURL: "https://gegt-alan8-18.firebaseio.com",
+    projectId: "gegt-alan8-18",
+    storageBucket: "gegt-alan8-18.appspot.com",
+    messagingSenderId: "868587304318"
+};
+firebase.initializeApp(config);
+
+var dataRef = firebase.database();
